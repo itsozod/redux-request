cf3275d2ab40629305ab9efc928ee5b178eb3813
Ozod <o.bukhorizoda@mail.ru>
2023-12-24 19:21:43 +0500

    made the request redux based and server sided

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,16 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-// import { getDatas } from "./store/reducers/getRequestReducer";
-// import { useDispatch, useSelector } from "react-redux";
-// const coffees = useSelector((state) => state.getRequestReducer.coffees);
-// const loader = useSelector((state) => state.getRequestReducer.loading);
-// const dispatch = useDispatch();
-// useEffect(() => {
-//   dispatch(getDatas());
-// }, [dispatch]);
+import { getDatas } from "./store/reducers/getRequestReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const [coffees, setCoffees] = useState([]);
   const [query, setQuery] = useState("");
-  const [loader, setLoader] = useState(false);
-
-  const getCoffees = async () => {
-    try {
-      setLoader(true);
-      const response = await fetch("http://localhost:3000/coffees");
-      const data = await response.json();
-      setCoffees(data);
-      setLoader(false);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
+  const coffees = useSelector((state) => state.getRequestReducer.coffees);
+  const loader = useSelector((state) => state.getRequestReducer.loading);
+  const dispatch = useDispatch();
   useEffect(() => {
-    getCoffees();
-  }, []);
-
-  const handleCoffees = (e) => {
-    const value = e.target.value;
-    setQuery(value);
-    if (value === "") {
-      getCoffees();
-    } else {
-      const filteredCoffees = coffees.filter((coffee) => {
-        return coffee.title.toLowerCase().includes(value.toLowerCase());
-      });
-      setCoffees(filteredCoffees);
-    }
-  };
+    dispatch(getDatas(query));
+  }, [dispatch, query]);
 
   return (
     <>
@@ -49,7 +18,7 @@ function App() {
         type="search"
         placeholder="Search..."
         value={query}
-        onChange={(e) => handleCoffees(e)}
+        onChange={(e) => setQuery(e.target.value)}
       />
       {loader ? (
         <p className="loader">Loading...</p>
--- src/store/reducers/getRequestReducer.js
+++ src/store/reducers/getRequestReducer.js
@@ -13,20 +13,19 @@ export const getRequestReducer = (state = requestData, action) => {
   }
 };
 
-export const getDatas = () => {
+export const getDatas = (query) => {
   return async (dispatch) => {
     try {
       dispatch({
         type: GET_REQUEST_ACTION,
         payload: { loading: true, coffees: [] },
       });
-      const reponse = await fetch("http://localhost:3000/coffees");
+      const reponse = await fetch(`http://localhost:3000/coffees?q=${query}`);
       const data = await reponse.json();
       dispatch({
         type: GET_REQUEST_ACTION,
         payload: { loading: false, coffees: data },
       });
-      // console.log("Coffees:", data);
     } catch (error) {
       dispatch({
         type: GET_REQUEST_ACTION,
